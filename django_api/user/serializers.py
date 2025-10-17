2546eee9f6e2b2521d0c180391c8e465ed91583a
鍵山社長 <104934091+kagiyama-baking@users.noreply.github.com>
2025-10-17 20:54:00 +0900

    トークン認証の追加

--- django_api/user/serializers.py
+++ django_api/user/serializers.py
@@ -16,3 +16,28 @@ class UserSerializer(serializers.ModelSerializer):
         user = get_user_model().objects.create_user(**validated_data)
 
         return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = ('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
--- django_api/user/views.py
+++ django_api/user/views.py
@@ -6,3 +6,8 @@ from user.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user, restrict who can see Todo"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
